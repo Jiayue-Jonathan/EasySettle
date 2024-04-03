475aca4370d55ef38965ca7e30143789eefad177
Jiayue-Jonathan <jonathanlv201710@gmail.com>
2024-04-03 10:55:19 -0700

    0403

--- EasySettle/Models/SearchCriteriaViewModel.cs
+++ EasySettle/Models/SearchCriteriaViewModel.cs
@@ -2,17 +2,21 @@ namespace EasySettle.Models;
 
 public class SearchCriteria
 {
-    public bool IsRoomsSearchActive { get; set; }
     public decimal? MinRooms { get; set; }
     public decimal? MaxRooms { get; set; }
 
-    public bool IsBathRoomsSearchActive { get; set; }
     public decimal? MinBathRooms { get; set; }
     public decimal? MaxBathRooms { get; set; }
 
-    public bool IsRentSearchActive { get; set; }
     public decimal? MinRent { get; set; }
     public decimal? MaxRent { get; set; }
 
-    // Add other properties as needed
+    public TypeEnum? Type { get; set; }
+
+    public CityEnum? City { get; set; }
+
+    public bool? Parking { get; set; }
+
+    public bool? Pets { get; set; }
+
 }
