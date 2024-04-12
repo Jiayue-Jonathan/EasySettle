d5e4bba6654e56b3a859d530263aa222e8d5c659
Jiayue-Jonathan <jonathanlv201710@gmail.com>
2024-04-12 16:18:58 -0700

    Your commit message

---
+++ EasySettle/EasySettle_Usr/Controllers/LeaseController.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EasySettle.Data;
+using EasySettle.Models;
+
+namespace EasySettle.Controllers
+{
+    public class LeaseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LeaseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lease
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Leases.Include(l => l.Client).Include(l => l.Property);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Lease/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Leases == null)
+            {
+                return NotFound();
+            }
+
+            var lease = await _context.Leases
+                .Include(l => l.Client)
+                .Include(l => l.Property)
+                .FirstOrDefaultAsync(m => m.LeaseID == id);
+            if (lease == null)
+            {
+                return NotFound();
+            }
+
+            return View(lease);
+        }
+
+        // GET: Lease/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "ClientID");
+            ViewData["PropertyID"] = new SelectList(_context.Properties, "PropertyID", "PropertyID");
+            return View();
+        }
+
+        // POST: Lease/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LeaseID,DepositPaid,RentStart,RentFinish,PropertyID,ClientID")] Lease lease)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lease);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "ClientID", lease.ClientID);
+            ViewData["PropertyID"] = new SelectList(_context.Properties, "PropertyID", "PropertyID", lease.PropertyID);
+            return View(lease);
+        }
+
+        // GET: Lease/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Leases == null)
+            {
+                return NotFound();
+            }
+
+            var lease = await _context.Leases.FindAsync(id);
+            if (lease == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "ClientID", lease.ClientID);
+            ViewData["PropertyID"] = new SelectList(_context.Properties, "PropertyID", "PropertyID", lease.PropertyID);
+            return View(lease);
+        }
+
+        // POST: Lease/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LeaseID,DepositPaid,RentStart,RentFinish,PropertyID,ClientID")] Lease lease)
+        {
+            if (id != lease.LeaseID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lease);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LeaseExists(lease.LeaseID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "ClientID", lease.ClientID);
+            ViewData["PropertyID"] = new SelectList(_context.Properties, "PropertyID", "PropertyID", lease.PropertyID);
+            return View(lease);
+        }
+
+        // GET: Lease/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Leases == null)
+            {
+                return NotFound();
+            }
+
+            var lease = await _context.Leases
+                .Include(l => l.Client)
+                .Include(l => l.Property)
+                .FirstOrDefaultAsync(m => m.LeaseID == id);
+            if (lease == null)
+            {
+                return NotFound();
+            }
+
+            return View(lease);
+        }
+
+        // POST: Lease/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Leases == null)
+            {
+                return Problem("Entity set 'AppDbContext.Leases'  is null.");
+            }
+            var lease = await _context.Leases.FindAsync(id);
+            if (lease != null)
+            {
+                _context.Leases.Remove(lease);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LeaseExists(int id)
+        {
+          return (_context.Leases?.Any(e => e.LeaseID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ EasySettle/EasySettle_Usr/Controllers/ManageController.cs
@@ -0,0 +1,169 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using EasySettle.Models;
+using EasySettle.Models.ViewModels;
+using EasySettle.Data;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization; 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models; // This is for PublicAccessType
+using Microsoft.EntityFrameworkCore; // This is for ToListAsync()
+using Microsoft.Extensions.Azure;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EasySettle.Controllers;
+
+public class ManageController : BaseController
+{
+    public ManageController(AppDbContext context, ILogger<HomeController> logger, BlobServiceClient blobServiceClient)
+        : base(context, logger, blobServiceClient)
+        {       
+        }
+    
+    public async Task<IActionResult> GetYourProperties()
+    {
+        var userEmail = User.Claims.FirstOrDefault(c => c.Type == "emails")?.Value;
+        if (string.IsNullOrEmpty(userEmail))
+        {
+            return Unauthorized("User is not authenticated.");
+        }
+
+        var owner = await _context.Owners
+                                .Include(o => o.Properties)
+                                .FirstOrDefaultAsync(o => o.Email == userEmail);
+
+        if (owner == null || owner.Properties == null || !owner.Properties.Any())
+        {
+            return NotFound("No properties found for this user.");
+        }
+
+        return View(owner.Properties.ToList());
+    }
+
+
+    // GET: Property/Create
+    public IActionResult PostYourProperty()
+    {
+        var userEmail = User.Claims.FirstOrDefault(c => c.Type == "emails")?.Value;
+        if (string.IsNullOrEmpty(userEmail))
+        {
+            return Unauthorized("Invalid user data.");
+        }
+
+        var isLandlord = User.Claims.Any(c => c.Type == "extension_Roles" 
+                                            && c.Value == "Landlord");
+        if (!isLandlord)
+        {
+            return Unauthorized("Access is restricted to landlords only.");
+        }
+
+        var viewModel = new PropertyAndOwnerViewModel();
+
+        var owner = _context.Owners.FirstOrDefault(o => o.Email == userEmail);
+        if (owner != null)
+        {
+            viewModel.Owner = owner;
+        }
+        else
+        {
+            int maxOwnerId = _context.Owners.Any() ? _context.Owners.Max(o => o.OwnerID) : 20000; // start IDs from 10000 if none exist
+            viewModel.Owner = new Owner { Email = userEmail, OwnerID = maxOwnerId + 1 };
+        }
+        int maxPropertyId = _context.Properties.Any() ? _context.Properties.Max(p => p.PropertyID) : 10000; // Default to 9999 if no properties exist
+        viewModel.Property = new Property { PropertyID = maxPropertyId + 1 };
+        return View(viewModel);
+    }
+
+
+   // POST: Property/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> PostYourProperty(PropertyAndOwnerViewModel viewModel)
+    {
+        var isLandlord = User.Claims.Any(c => c.Type == "extension_Roles" && c.Value == "Landlord");
+        if (!isLandlord)
+        {
+            return Unauthorized();
+        }
+
+        if (ModelState.IsValid)
+        {
+            // Ensure the Owner is added and saved first if it's new
+            if (!_context.Owners.Any(o => o.OwnerID == viewModel.Owner.OwnerID))
+            {
+                _context.Owners.Add(viewModel.Owner);
+                await _context.SaveChangesAsync(); // This ensures OwnerID is generated
+            }  else
+            {
+                _context.Update(viewModel.Owner); // Update existing owner
+                await _context.SaveChangesAsync();
+            }
+
+            viewModel.Property.OwnerID = viewModel.Owner.OwnerID; // Ensure the correct OwnerID is assigned
+            _context.Properties.Add(viewModel.Property);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("UploadPhoto", "Property", new { id = viewModel.Property.PropertyID });
+        }
+
+        return View(viewModel);
+    } 
+    public IActionResult Profile()
+    {
+        var model = new UserProfileViewModel
+        {
+            Email = User.Claims.FirstOrDefault(c => c.Type == "emails")?.Value,
+            DisplayName = User.Claims.FirstOrDefault(c => c.Type == "name")?.Value,
+            ObjectId = User.Claims.FirstOrDefault(c => c.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")?.Value,
+            City = User.Claims.FirstOrDefault(c => c.Type == "city")?.Value, 
+            Roles = User.Claims.FirstOrDefault(c => c.Type == "extension_Roles")?.Value,
+            GivenName = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")?.Value,
+
+        };
+
+        return View("Profile",model);
+    }
+
+    [Authorize]
+    public async Task<IActionResult> YourList()
+    {
+        var userEmail = User.Claims.FirstOrDefault(c => c.Type == "emails")?.Value;
+
+        if (string.IsNullOrEmpty(userEmail))
+        {
+            return Unauthorized("You must be logged in to view your properties.");
+        }
+
+        var userProperties = await _context.UserProperties
+            .Where(up => up.Email == userEmail)
+            .Select(up => up.PropertyID)
+            .ToListAsync();
+
+        var properties = await _context.Properties
+            .Where(p => userProperties.Contains(p.PropertyID) && p.IsAudited)
+            .ToListAsync();
+
+        var propertyViewModels = await GetPropertyViewModelsAsync(properties);
+
+        return View(propertyViewModels);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> HandlePropertyToggle(int propertyId, bool isChecked)
+    {
+        // Define your redirection targets
+        var redirectToAction = "YourList";
+        var redirectToController = "Manage";
+
+        // Correctly await the call to the asynchronous method
+        return await ToggleUserProperty(propertyId, isChecked, redirectToAction, redirectToController);
+    }    
+
+}
+
+
